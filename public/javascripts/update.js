25ae09a53f093e5a46b7db6117ff0bcb591894f6
Evan Prodromou <evan@fuzzy.io>
2015-03-28 17:26:24 -0400

    Update the table

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -3,23 +3,38 @@ var startSpinner = function() {}
 var stopSpinner = function() {}
 
 var updateTable = function(data) {
-  console.log(JSON.stringify(data));
+  $("table#t01").empty();
+  $("table#t01").append("<tr><th>Team Name</th><th>Score</th>");
+  var pairs = _.pairs(data);
+  pairs.sort(function(a, b) {
+    if (a[1] < b[1]) {
+      return 1;
+    } else if (a[1] > b[1]) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  _.each(pairs, function(pair) {
+    $("table#t01").append("<tr><td>"+pair[0]+"</td><td>"+pair[1]+"</td>");
+  });
 }
 
 var getSliderValues = function() {
   var values = {
-    "K-BB%": "medium",
-    "WHIP": "ignore",
-    "ERA": "low",
-    "RA9-WAR": "high",
-    "OPS": "medium",
-    "Spd": "low",
-    "R-diff": "high"
+    "K-BB%": $("input:radio[name='kbb']:checked").val(),
+    "WHIP": $("input:radio[name='whip']:checked").val(),
+    "ERA": $("input:radio[name='era']:checked").val(),
+    "RA9-WAR": $("input:radio[name='ra9war']:checked").val(),
+    "OPS": $("input:radio[name='ops']:checked").val(),
+    "Spd": $("input:radio[name='spd']:checked").val(),
+    "R-diff": $("input:radio[name='rdiff']:checked").val()
   };
   return values;
 };
 
 $(document).ready(function() {
+  $("input[type='radio']").on("click", function() {
     console.log("DOING IT");
     var values = getSliderValues();
     console.log(JSON.stringify(values));
@@ -28,4 +43,5 @@ $(document).ready(function() {
       stopSpinner();
       updateTable(data);
     });
+  });
 });
